0f6d057a8dd232f7a017d8edc1eb7511fe0da7c5
heba907 <helaz.perso@gmail.com>
2026-04-17 17:08:34 +0100

    blochain hash (sepolia-remixide-alchemy)

--- custom_addons/mecanicien/services/blockchain_service.py
+++ custom_addons/mecanicien/services/blockchain_service.py
@@ -1,18 +1,35 @@
 import hashlib
-import uuid
 import json
 import os
 
+from web3 import Web3
+from dotenv import load_dotenv
 
-CONTRACT_ADDRESS = "SIMULATED_BLOCKCHAIN"
+load_dotenv()
+
+CONTRACT_ADDRESS = "0x2b32e3677BC816A68e4fd17823a472cAb5D186A0"
 
 
 def get_web3():
-    return None
+    rpc_url = os.getenv("ALCHEMY_URL")
+    return Web3(Web3.HTTPProvider(rpc_url))
 
 
 def get_contract():
-    return None
+    web3 = get_web3()
+
+    abi_path = os.path.join(
+        os.path.dirname(__file__),
+        "GarageRegistry_abi.json"
+    )
+
+    with open(abi_path, "r") as f:
+        abi = json.load(f)
+
+    return web3.eth.contract(
+        address=Web3.to_checksum_address(CONTRACT_ADDRESS),
+        abi=abi
+    )
 
 
 def compute_reparation_hash(reparation):
@@ -23,7 +40,7 @@ def compute_reparation_hash(reparation):
 def enregistrer_sur_blockchain(reparation):
     hash_value = compute_reparation_hash(reparation)
 
-    # simulation d'un transaction hash type blockchain
-    tx_hash = "0x" + uuid.uuid4().hex + uuid.uuid4().hex[:24]
+    # temporaire : test de connexion
+    tx_hash = "CONNECTED_TO_SEPOLIA"
 
     return tx_hash, hash_value
